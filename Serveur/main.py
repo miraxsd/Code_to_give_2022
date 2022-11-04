509b48f049a2e4bbd5af4c617edd08263829c668
miraxsd <miraxsd@kali.hitronhub.home>
2022-11-03 23:49:41 -0400

    Add keywords extraction in server

--- Serveur/main.py
+++ Serveur/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import spacy
 
 app = Flask(__name__)
 
@@ -6,3 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+# Source: https://towardsdatascience.com/keyword-extraction-process-in-python-with-natural-language-processing-nlp-d769a9069d5c
+# Returns a tuple of keywords
+
+def extract_keywords(text, language):
+    if language == "en":
+        nlp = spacy.load("en_core_web_sm") ## To choose the right loaded package for our case
+    else:
+        nlp = spacy.load("fr_core_news_sm")
+    doc = nlp(text)
+    return doc.ents
