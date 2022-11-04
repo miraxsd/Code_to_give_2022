426f0bbcf374f3b3ee22b0adde5b33d9707d0998
miraxsd <miraxsd@kali.hitronhub.home>
2022-11-04 02:39:02 -0400

    Add mongoDB connection to server

--- Serveur/main.py
+++ Serveur/main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+
+from flask import Flask, current_app, g
 import spacy
+from database import MongoAPI
+from flask_pymongo import PyMongo
+from configparser import ConfigParser
+import os
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
+    #print(mongo_obj.read())
     return 'Hello, World!'
+
+
+
 
 # Source: https://towardsdatascience.com/keyword-extraction-process-in-python-with-natural-language-processing-nlp-d769a9069d5c
 # Returns a tuple of keywords
@@ -18,3 +27,12 @@ def extract_keywords(text, language):
         nlp = spacy.load("fr_core_news_sm")
     doc = nlp(text)
     return doc.ents
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
+    
+    data = {
+        "database": "CodeToGive",
+        "collection": "posts",
+    } 
+    mongo_obj = MongoAPI(data)
