575a41e5edbb738068342fdde10c2f8526294720
miraxsd <miraxsd@kali.hitronhub.home>
2022-11-04 19:00:55 -0400

    Add mongoDB clean version

--- Serveur/main.py
+++ Serveur/main.py
@@ -1,17 +1,13 @@
 
 from flask import Flask, current_app, g
 import spacy
-from database import MongoAPI
-from flask_pymongo import PyMongo
-from configparser import ConfigParser
-import os
+from mongoengine import *
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    #print(mongo_obj.read())
     return 'Hello, World!'
 
 
@@ -30,9 +26,4 @@ def extract_keywords(text, language):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
-    
-    data = {
-        "database": "CodeToGive",
-        "collection": "posts",
-    } 
-    mongo_obj = MongoAPI(data)
+    connect(host="mongodb://Admin:Admin@cluster0.ssnkw.mongodb.net/?retryWrites=true&w=majority")
