519819f2148e7a41adb1e447e89cb461fc714abb
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-11-04 20:49:57 -0400

    added new pages and setup of navbar and background components, as well as app css rules

---
+++ Client/src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/NavBar/NavBar'
+import '../pages/Login.scss'
+
+const Login = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <NavBar></NavBar>
+      </header>
+    </div>
+  )
+}
+
+export default Login
---
+++ Client/src/pages/Welcome.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/NavBar/NavBar'
+import '../pages/Welcome.scss'
+
+const Welcome = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <NavBar></NavBar>
+      </header>
+    </div>
+  )
+}
+
+export default Welcome
