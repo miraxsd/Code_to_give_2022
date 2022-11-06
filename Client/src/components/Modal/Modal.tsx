7e71be258a743da39fcdc5efbf11f2d8afedfca9
miraxsd <60142429+miraxsd@users.noreply.github.com>
2022-11-05 21:30:33 -0400

    Add first modal of welcome page

--- Client/src/components/Modal/Modal.tsx
+++ Client/src/components/Modal/Modal.tsx
@@ -1,34 +1,18 @@
 import React from 'react'
+import { JsxAttribute } from 'typescript';
 import '../Modal/Modal.scss'
-import { RiCloseLine } from "react-icons/ri";
 
-const Modal = () => {
+interface ModalProps {
+  children?: JSX.Element,
+}
+
+const Modal = ({children}: ModalProps) => {
   return (
     <>
       <div className='darkBG'/>
       <div className='centered'>
         <div className='modal'>
-          <div className='modalHeader'>
-            <h5 className='heading'>Dialog</h5>
-          </div>
-          <button className='closeBtn'>
-            <RiCloseLine style={{ marginBottom: "-3px" }} />
-          </button>
-          <div className='modalContent'>
-            Are you sure you want to delete the item?
-          </div>
-          <div className='modalActions'>
-            <div className='actionsContainer'>
-              <button className='deleteBtn'>
-                Delete
-              </button>
-              <button
-                className='cancelBtn'
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
+          {children}
         </div>
       </div>
     </>
--- Client/src/pages/Welcome.tsx
+++ Client/src/pages/Welcome.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NavBar'
+import Modal from '../components/Modal/Modal'
 import '../pages/Welcome.scss'
+import '../components/Modal/Modal.scss'
+
+let image1 = require('../assets/Solidaire.png');
+let image2 = require('../assets/eleves.png');
+let image3 = require('../assets/entrreaid.png');
 
 const Welcome = () => {
   return (
     <div>
       <header className="App-header">
         <NavBar></NavBar>
+        <Modal>
+          <div>
+          <div className='modalHeader'>
+            <h5 className='heading'>Welcome to "Digital forum"</h5>
+          </div>
+          
+          <div className='modalContent'>
+          A place where you can share and discuss issues in your community and communities around the world
+          </div>
+          <button className="centered button4">Let's get started</button>
+          <div></div>
+          <div className="row">
+            <div className="column">
+              <img className='image' src={image3} alt=""></img>
+            </div>
+            <div className="column">
+              <img className='image' src={image2} alt=""></img>
+            </div>
+            <div className="column">
+              <img className='image' src={image1} alt=""></img>
+            </div>
+            </div>
+          </div>
+          
+            
+          </Modal>
       </header>
     </div>
   )
