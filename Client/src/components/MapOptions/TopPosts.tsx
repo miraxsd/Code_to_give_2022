88416ea711711b4ccdf8e7881a42e8afadaae10e
Bryan-Tad <bryan.tadie@polymtl.ca>
2022-11-05 22:36:13 -0400

    Structure du post complété

--- Client/src/components/MapOptions/TopPosts.tsx
+++ Client/src/components/MapOptions/TopPosts.tsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import '../MapOptions/TopPosts.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import {  } from '@fortawesome/free-solid-svg-icons'
+import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
+
+let user = require('../../assets/user.png');
 
 const TopPosts = () => {
   return (
     <div className='top-posts'>
-      <div className='posts-list'>
-        TopPosts
-      </div>
+      <h1>Top posts in this area</h1>
+      <PostItem/>
     </div>
   )
 }
+
+const PostItem = () => {
+  return (
+    <div className='posts-list'>
+        <h2 className='post-title'>No access to healthy restaurants in my area</h2>
+        <span className='post-type'>Issue</span>
+        <span className='post-author'>RandomUser</span>
+        <img className='profile-picture' src={user} alt='profile pic'></img>
+        <span className='post-date'>10 weeks ago</span>
+        <div className='post-description'>
+            <p>I think its really unfortunate that there are no good restaurant in my area.</p>
+        </div>
+        <button className='post-like'><FontAwesomeIcon icon={faHeart} /></button><span className='likes-number'>730</span>
+        <button className='post-comment'><FontAwesomeIcon icon={faComment} /></button><span className='post-reply'>Reply</span>
+    </div>
+  )
+}
+
+//const buildPostItem = function (object: post) {
+
+//}
 
 export default TopPosts
---
+++ Client/src/components/MapOptions/TopPostsList.js
@@ -0,0 +1,49 @@
+export const randomPosts = [
+    {
+        id: 0,
+        title: "Not enough public transport access",
+        author: "Random Guy",
+        date: "5 days ago",
+        description: "I feel like there is not enough buses in my area",
+        liked: false,
+        nLikes: 103
+    },
+    {
+        id: 1,
+        title: "KinderGanden too expensive",
+        author: "Random Mom",
+        date: "5 days ago",
+        description: "I'm a mom and sending my kid to kindergarden is so expensive. How can we fix that",
+        liked: true,
+        nLikes: 759
+    },
+    {
+        id: 2,
+        title: "I challenge everyDay to sing a love song to their wife",
+        author: "Random Dad",
+        date: "2 weeks ago",
+        description: "Every husband should take the time to sing a wonderful song to their wife",
+        liked: false,
+        nLikes: 5
+    },
+    {
+        id: 3,
+        title: "School dressing Code",
+        author: "Random Girl",
+        date: "1 month ago",
+        description: "The dress code is unfair for girl",
+        liked: true,
+        nLikes: 999
+    },
+    {
+        id: 4,
+        title: "Against exams",
+        author: "Random student",
+        date: "1 days ago",
+        description: "I feel like exams are useless. We should stop having exams and change the gradin system.",
+        liked: false,
+        nLikes: 145
+    }
+];
+
+export default randomPosts;
