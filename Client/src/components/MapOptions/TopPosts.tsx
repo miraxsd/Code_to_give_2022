f4e94a7508d8d1cf0aef3a9e6a5335b5a7f275a4
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-11-06 23:15:10 -0500

    loads with search box and locate button

--- Client/src/components/MapOptions/TopPosts.tsx
+++ Client/src/components/MapOptions/TopPosts.tsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { MutableRefObject, useEffect, useState } from 'react'
 import '../MapOptions/TopPosts.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import PostItem from '../MapOptions/PostItem'
 import posts from './Posts'
+import eventBus from '../../eventBus'
 
-const TopPosts = () => {
+interface TopPostsProps {
+  mapRef: MutableRefObject<any>
+}
+
+const TopPosts = ({mapRef}:TopPostsProps) => {
 
   const [details, setDetails] = useState([{
     id: 0,
@@ -12,24 +17,61 @@ const TopPosts = () => {
     location: [17.966958, -66.123551],
     postType: "idea",
     etiquettes: ['Friendship', 'School-life'],
-    title: "Hurricane hit my city",
     description :
     "Last month, a hurricane hit my city and destroyed everything. My family was lucky, and our house was not really damaged, but most of my friends’ houses are gone. The power just came back, but we still don’t have clean water and food is hard to find. I don’t know how to help my community.",
-    date: "4 days ago",
-    liked: false,
-    numberOfLikes: 110,
-    comments: [{
-        user: "juan908",
-        location: [17.934013, -76.456299],
-        comment:
-        "I was in this situation last year, so I know it’s really hard. You can help by sharing some of your clothes and utilities with your friends who lost everything. Even if it’s just a couple of shirts, it will help a lot.",
-        liked: false,
-        numberOfLikes: 40
-    }]
+    numberOfLikes: 110
   }])
+
+  eventBus.on('newLocation', (e: any) => {
+    console.log('event', e)
+    getPosts(e);
+  });
+
+  const getPosts = async (location: any) => {
+    console.log(mapRef.current)
+    await fetch(`/api/posts`, {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({
+          location: [location.lat, location.lng],
+          etiquette: ['Climate']
+      })
+
+    }).then((response: Response) => response.json()).then((data: any) => {
+      let list: any[] =  [];
+      data.map((post: any) => 
+          list.push(
+              {
+                  id: post._id.$oid,
+                  user: post.user,
+                  location: post.location,
+                  postType: post.postType,
+                  etiquettes: post.etiquettes,
+                  description : post.text,
+                  numberOfLikes: post.numberOfLikes
+              }
+          )
+      )
+      console.log('list ', list)
+      setDetails(list);
+    }).catch(()=>{
+      setDetails(current => [...current, {
+        id: 0,
+        user:  "Bobby123",
+        location: [17.966958, -66.123551],
+        postType: "idea",
+        etiquettes: ['Friendship', 'School-life'],
+        description :
+        "Last month, a hurricane hit my city and destroyed everything. My family was lucky, and our house was not really damaged, but most of my friends’ houses are gone. The power just came back, but we still don’t have clean water and food is hard to find. I don’t know how to help my community.",
+        numberOfLikes: 110
+      
+      }]);
+    })
+  }
 
   useEffect(() => {
     //Insérer get top posts
+      getPosts({lat: 17.966958, lng: -66.123551});
   }, [])
 
   return (
